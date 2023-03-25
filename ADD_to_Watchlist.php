2e32d8f3d3fab1bd7e01f4871938d4ea57fb3f22
PyaeSone98 <107252632+PyaeSone98@users.noreply.github.com>
2023-03-25 22:42:40 +0630

    pp

---
+++ ADD_to_Watchlist.php
@@ -0,0 +1,37 @@
+<?php
+include_once 'core.php';
+include 'dbconnect.php';
+
+// Sanitize the user ID and entity ID from the request
+// $user_id = filter_input(INPUT_GET, 'user_id', FILTER_SANITIZE_NUMBER_INT);
+// $entity_id = filter_input(INPUT_GET, 'entity_id', FILTER_SANITIZE_NUMBER_INT);
+
+$user_id = $_GET['user_id'];
+$entity_id=$_GET['entity_id'];
+
+// Check if user ID and entity ID are valid
+if (!$user_id || !$entity_id) {
+    header("HTTP/1.0 400 Bad Request");
+    echo json_encode(array('error' => 'Invalid user or entity'));
+    exit();
+}
+
+// Prepare a statement to insert a new row into the watchlist table
+$insert_query = "INSERT INTO watchlist (user_id, entity_ID) VALUES (?, ?)";
+$stmt = $db->prepare($insert_query);
+$stmt->bind_param("ii", $user_id, $entity_id);
+// Execute the statement and check for errors
+
+if ($stmt->execute()) {
+    // Return success message as JSON
+    echo json_encode(array('success' => 'Added to watchlist'));
+} else {
+    
+         // Return generic error message as JSON
+        header("HTTP/1.0 500 Internal Server Error");
+        echo json_encode(array('error' => 'Could not add to watchlist'));
+    
+}
+
+$stmt->close();
+?>
---
+++ latest_movie.php
@@ -0,0 +1,39 @@
+<?php 
+include_once 'core.php';
+include 'dbconnect.php';
+
+
+$latest_entity_query = "SELECT id,is_movie FROM entity ORDER BY date_added DESC LIMIT 1";
+$latest_entity_result = $db->query($latest_entity_query);
+
+ 
+
+ if($latest_entity_result->num_rows>0) {
+    $last_inserted_row= $latest_entity_result->fetch_assoc();
+}
+
+    
+$id = $last_inserted_row['id'];
+$check_entity_type=$last_inserted_row['is_movie'];
+
+if($check_entity_type==1) {
+    $lastest_movie_query = "SELECT * FROM videos WHERE entity_ID=$id";
+    $lastest_movie_result = $db->query($lastest_movie_query);
+
+    if($lastest_movie_result->num_rows>0){
+    $lastest_movie_data=$lastest_movie_result->fetch_assoc();
+        $movie_data=$lastest_movie_data;
+
+    }
+    echo json_encode($movie_data);
+}else {
+    $lastest_tvshow_query ="SELECT * FROM episodes WHERE entity_ID=$id";
+    $latest_tvshow_result = $db->query($lastest_tvshow_query);
+
+    if($latest_tvshow_result->num_rows>0){
+        $latest_tvshow_data=$latest_tvshow_result->fetch_assoc();
+        $tvshow_data=$latest_tvshow_data;
+    }
+    echo json_encode($tvshow_data);
+
+}
