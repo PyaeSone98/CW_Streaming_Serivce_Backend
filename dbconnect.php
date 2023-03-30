ee0ae5f894b6c91ef219eb16ec56a3fc32dbf724
PyaeSone98 <107252632+PyaeSone98@users.noreply.github.com>
2023-03-31 00:02:10 +0630

    p

--- dbconnect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$db = mysqli_connect("localhost","root","","code-worms_db");
-
-//check connection
-if (mysqli_connect_error()) {
-    echo "Failed to connect to MYSQL".mysqli_connect_error();
-
-}
