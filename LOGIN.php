10fdee823a9a0fb23b93500f8ac5bd8f8d31834c
PyaeSone98 <107252632+PyaeSone98@users.noreply.github.com>
2023-04-01 10:15:19 +0630

    Update LOGIN.php

--- LOGIN.php
+++ LOGIN.php
@@ -1,7 +1,12 @@
 <?php
 include "dbconnect.php";
-include_once "core.php";
+
 require_once 'vendor/autoload.php';
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: POST");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 
 use Firebase\JWT\JWT;
